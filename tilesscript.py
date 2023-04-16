512133fb4d46fc89c44cf67615f2fad4dcab3b88
dhritimamtora <117092024+dhritimamtora@users.noreply.github.com>
2023-04-16 01:03:37 -0400

    Add files via upload

---
+++ tilesscript.py
@@ -0,0 +1,65 @@
+
+#this script takes the path to the directory with all the files 
+#and then tiles all the .mrvs images in the directory
+#then it saves the tiled images into new folders organized by levels
+#also inverts the images
+
+
+from openslide import open_slide
+import openslide
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+import os
+import glob
+from openslide.deepzoom import DeepZoomGenerator
+
+#enter the path
+path = input("Enter path: ")
+
+#list of files that need to be tiled
+files = glob.glob(path + '*.mrxs')
+#create new directory to add all the tiled images
+directory = "tiled_images"
+new_path = os.path.join(path, directory)
+os.mkdir(new_path)
+
+#iterate through all the files
+for file in files:
+    #opens the file
+    slide = open_slide(file)
+    #creates the tiles
+    tiles = DeepZoomGenerator(slide, tile_size=500, overlap=0, limit_bounds=False)
+    #finds how many levels in tiles
+    levels = tiles.level_count
+    #actual file name
+    filename = file.split('.')
+    filename_new = filename[0].split('/')
+    name = len(filename_new) - 1
+    filename_final = filename_new[name]
+    newer_path = os.path.join(new_path, filename_final)
+    os.mkdir(newer_path)
+    #iterate through each level and saves tiles
+    for l in range(levels):
+        cols, rows = tiles.level_tiles[l]
+        new_path_folder = newer_path+'/level_'+str(l)
+        os.mkdir(new_path_folder)
+        for row in range(rows):
+            for col in range(cols):
+                tile_name = os.path.join(new_path_folder,'%d_%d' % (col, row))
+                print("Now saving tile with title: ", tile_name)
+                temp_tile = tiles.get_tile(l, (col, row))
+                temp_tile_RGB = temp_tile.convert('RGB')
+                temp_tile_np = np.array(temp_tile_RGB)
+                #grayscale conversion
+                R, G, B = temp_tile_np[:,:,0], temp_tile_np[:,:,1], temp_tile_np[:,:,2]
+                img_gray = 0.2989 * R + 0.5870 * G + 0.1140 * B
+                #invert image
+                img_inverted = 255 - img_gray
+                plt.imsave(tile_name + ".tiff", img_inverted)
+  
+
+        
+        
+    
