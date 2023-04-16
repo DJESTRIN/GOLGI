c49e590bf1cb7a681e1b676b2bb822b87ef00800
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-04-16 09:29:59 -0400

    Update tilesscript.py

--- tilesscript.py
+++ tilesscript.py
@@ -13,55 +13,56 @@ import os
 import glob
 from openslide.deepzoom import DeepZoomGenerator
 import tqdm
+import argparser
 
-#enter the path
-path = input("Enter path: ")
+# primary function for conversion
+def main(path):
+    #list of files that need to be tiled
+    files = glob.glob(path + '*.mrxs')
+    #create new directory to add all the tiled images
+    directory = "tiled_images"
+    new_path = os.path.join(path, directory)
+    os.mkdir(new_path)
 
-#list of files that need to be tiled
-files = glob.glob(path + '*.mrxs')
-#create new directory to add all the tiled images
-directory = "tiled_images"
-new_path = os.path.join(path, directory)
-os.mkdir(new_path)
+    #iterate through all the files
+    for file in files:
+        #opens the file
+        slide = open_slide(file)
+        #creates the tiles
+        tiles = DeepZoomGenerator(slide, tile_size=500, overlap=0, limit_bounds=False)
+        #finds how many levels in tiles
+        levels = tiles.level_count
+        #actual file name
+        filename = file.split('.')
+        filename_new = filename[0].split('/')
+        name = len(filename_new) - 1
+        filename_final = filename_new[name]
+        newer_path = os.path.join(new_path, filename_final)
+        os.mkdir(newer_path)
+        #iterate through each level and saves tiles
+        for l in tqdm.tqdm(range(levels)):
+            cols, rows = tiles.level_tiles[l]
+            new_path_folder = newer_path+'/level_'+str(l)
+            os.mkdir(new_path_folder)
+            for row in range(rows):
+                for col in range(cols):
+                    tile_name = os.path.join(new_path_folder,'%d_%d' % (col, row))
+                    print("Now saving tile with title: ", tile_name)
+                    temp_tile = tiles.get_tile(l, (col, row))
+                    temp_tile_RGB = temp_tile.convert('RGB')
+                    temp_tile_np = np.array(temp_tile_RGB)
 
-#iterate through all the files
-for file in files:
-    #opens the file
-    slide = open_slide(file)
-    #creates the tiles
-    tiles = DeepZoomGenerator(slide, tile_size=500, overlap=0, limit_bounds=False)
-    #finds how many levels in tiles
-    levels = tiles.level_count
-    #actual file name
-    filename = file.split('.')
-    filename_new = filename[0].split('/')
-    name = len(filename_new) - 1
-    filename_final = filename_new[name]
-    newer_path = os.path.join(new_path, filename_final)
-    os.mkdir(newer_path)
-    #iterate through each level and saves tiles
-    for l in tqdm.tqdm(range(levels)):
-        cols, rows = tiles.level_tiles[l]
-        new_path_folder = newer_path+'/level_'+str(l)
-        os.mkdir(new_path_folder)
-        for row in range(rows):
-            for col in range(cols):
-                tile_name = os.path.join(new_path_folder,'%d_%d' % (col, row))
-                print("Now saving tile with title: ", tile_name)
-                temp_tile = tiles.get_tile(l, (col, row))
-                temp_tile_RGB = temp_tile.convert('RGB')
-                temp_tile_np = np.array(temp_tile_RGB)
-                
-                # Convert to gray scale:
-                img_gray=np.mean(temp_tile_np,axis=2)
-                #R, G, B = temp_tile_np[:,:,0], temp_tile_np[:,:,1], temp_tile_np[:,:,2]   Does this work?
-                #img_gray = 0.2989 * R + 0.5870 * G + 0.1140 * B       Does this work?
-                
-                #invert image
-                img_inverted = 255 - img_gray
-                plt.imsave(tile_name + ".tiff", img_inverted)   #will this down sample image?
+                    # Convert to gray scale:
+                    img_gray=np.mean(temp_tile_np,axis=2)
+                    #R, G, B = temp_tile_np[:,:,0], temp_tile_np[:,:,1], temp_tile_np[:,:,2]   Does this work?
+                    #img_gray = 0.2989 * R + 0.5870 * G + 0.1140 * B       Does this work?
+
+                    #invert image
+                    img_inverted = 255 - img_gray
+                    plt.imsave(tile_name + ".tiff", img_inverted)   #will this down sample image?
   
-
+if __name__=='__main__':
+    
         
         
     
