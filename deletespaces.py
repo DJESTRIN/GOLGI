88dea8cf5b21dfb962bead68d5bc19a4530341d9
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-04-18 17:31:41 -0400

    Update deletespaces.py

--- deletespaces.py
+++ deletespaces.py
@@ -1,8 +1,19 @@
-def delete_spaces():
-    import os
-    directory = input('type the path to your folder')
+#!/bin/python
+"Dependencies"
+import os
+import argparse 
+
+def delete_spaces(directory):
     os.chdir(directory)
     path = os.getcwd()
     filenames = os.listdir(path)
     for filename in filenames:
         os.rename(os.path.join(path,filename),os.path.join(path,filename.replace(' ','')))
+        
+if __name__=="__main__":
+   # Command line interface
+  parser = argparse.ArgumentParser(description="Remove spaces from filenames and folders ")
+  parser.add_argument("--input", type=str, help="The directory containing the input mrxs files ",required=True)
+  args = parser.parse_args()
+  delete_spaces(args.input)
+    
