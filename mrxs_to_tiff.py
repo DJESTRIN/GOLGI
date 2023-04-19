ceb2dcc992ccb8acb7530f008c7e66ca679e6313
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-04-19 16:01:59 -0400

    Update mrxs_to_tiff.py

--- mrxs_to_tiff.py
+++ mrxs_to_tiff.py
@@ -42,6 +42,7 @@ def mrxstotiff(input_dir):
                     # Save the tile as a TIFF file
                     tile_filename = f'{os.path.splitext(filename)[0]}_{i}_{j}.tiff'
                     fulldrop_path=output_dir+filename[-6]+'tiled_images/'
+                    ipdb.set_trace()
                     os.mkdir(fulldrop_path,exist_ok=True)
                     tile.save(os.path.join(fulldrop_path,tile_filename), format='TIFF')
                     print(tile_filename)
